c7ac815ec44cc3b6922a3ae7d7b6a47c122e3bea
ramesh-2003-krishan <sudarshanakrishan123@gmail.com>
2026-09-02 22:08:59 +0530

    created project features create project request Dto

---
+++ backend/src/TaskPRO.Application/features/Projects/DTOs/CreateProjectRequest.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskPRO.Application.features.Projects
+{
+    public class CreateProjectRequest
+    {
+        [Required]
+        public string ProjectName {get; set;} = string.Empty;
+
+        [Required]
+        public string Description { get; set;} = string.Empty;
+
+        [Required]
+        public DateTime StartDate {get; set;}
+
+        [Required]
+        public DateTime EndDate {get; set;}
+    }
+}
